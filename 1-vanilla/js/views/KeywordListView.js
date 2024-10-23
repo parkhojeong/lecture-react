4444bd8061e897938ea0a90ecf8cb7f3921a31a2
parkhojeong <parkhj062@gmail.com>
2024-10-23 14:13:32 +0900

    추천 검색어2 - 목록에서 검색어 클릭하면 선택된 검색어의 검색 결과 화면으로 이동한다.(검색어 입력란에 선택된 검색어 적어주는 기능은 제외)

--- 1-vanilla/js/views/KeywordListView.js
+++ 1-vanilla/js/views/KeywordListView.js
@@ -1,4 +1,4 @@
-import { qs } from "../helpers.js";
+import {delegate, qs} from "../helpers.js";
 import View from "./View.js";
 
 const tag = "[KeywrodListView]";
@@ -10,15 +10,17 @@ export default class KeywordListView extends View {
     super(qs("#keyword-list-view"));
 
     this.template = new Template();
-    // TODO
+    this.bindEvents();
   }
 
   bindEvents() {
-    // TODO
+    delegate(this.element, "click", "li", event => this.handleClick(event));
   }
 
-  handleClick() {
-    // TODO
+  handleClick(event) {
+    const value = event.target.dataset.keyword;
+    console.log(tag, "handleClick", value);
+    this.emit("@click", {value})
   }
 
   show(data = []) {
